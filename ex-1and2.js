5212de6482e7642f4ce05413676621412d0cc1cb
BBTC CTBB <enterpannet@gmail.com>
2023-11-19 18:52:38 +0700

    Do HH Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,20 @@
 //Exercise #1: Variable Creation
 //Start coding here
+//1.1
+let firstName = "Karter";
+//1.2
+let lastName = "Anderson";
+//1.3
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+//1.4
+const nameRestaurant = "Chef's Table by Chef James";
+//1.5
+const netPrice = 15000;
+//1.6
+const totalActiveRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = "(970) 878-53292400 County Rd#4Meeker, Colorado(CO),81641";
+netPrice = 5000;
+totalActiveRestaurant = 2500;
