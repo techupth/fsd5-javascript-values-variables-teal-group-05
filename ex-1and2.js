c5c73f1b34314ffb960d89de01d97b27ba583786
Panupong <Pai28212m@gmail.com>
2023-11-23 14:00:56 +0700

    HH:Pai

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,18 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const storeName = "Chef's Table by Chef James";
+const billPrice = 15000;
+const availableStore = 1500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+billPrice = 5000;
+availableStore = 2500
